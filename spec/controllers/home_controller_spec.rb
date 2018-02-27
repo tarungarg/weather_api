3d44c8287e1be8a87b86ea782def18cb2f06d6a6
Tarun <t.garg28@gmail.com>
2018-02-27 16:19:33 +0530

    more test cases

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,13 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
-  context "GET #index" do
-    it "should populate weather data" do
-      VCR.use_cassette("weather_api") do
-        get :index
-        expect(response).to render_template("index")
+
+  describe HomeController do
+    let(:client) { WeatherApi.http_get }
+
+    context "GET #index" do
+      it "should populate weather data" do
+        VCR.use_cassette("weather_api") do
+          get :index
+          expect(response).to render_template("index")
+        end
+      end
+
+      it "should check weather data" do
+        expect(client.first["date"]).to eq("2018-02-27")
+      end
+
+      it "should check array data" do
+        expect(client).to be_an_instance_of(Array)
       end
     end
-
   end
+
 end
