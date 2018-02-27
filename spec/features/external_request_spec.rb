c413cc94a0162c2cc4ab10f5a816f89e27943d01
Tarun <t.garg28@gmail.com>
2018-02-27 16:03:09 +0530

    added rspec

--- spec/features/external_request_spec.rb
+++ spec/features/external_request_spec.rb
@@ -1,11 +1,26 @@
 require "rails_helper"
+require "rspec/expectations"
 
 RSpec.feature "Widget management", :type => :feature do
-  it 'queries weather site for fetching 1 day data' do
-    uri = URI('http://api.worldweatheronline.com/premium/v1/weather.ashx')
 
-    response = JSON.load(Net::HTTP.get(uri))
-    expect(response["data"]["weather"].count).to eq 1
+  describe "API hit to weather" do
+
+    before(:each) do
+      uri = URI('http://api.worldweatheronline.com/premium/v1/weather.ashx')
+      @response = JSON.load(Net::HTTP.get(uri))
+    end
+
+    it 'will fetch 1 day data' do
+      expect(@response["data"]["weather"].count).to eq 1
+    end
+
+    it 'has some data in request' do
+      expect(@response["data"]["weather"]).not_to be_empty
+    end
+
+    it 'queries weather data have right key' do
+      expect(@response["data"]).to have_key('weather')
+    end
   end
 
 end
