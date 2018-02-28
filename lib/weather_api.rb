ad850393809def034ad79950806fda6f28bcb3f1
Tarun <t.garg28@gmail.com>
2018-02-28 12:09:22 +0530

    added keys to secret

--- lib/weather_api.rb
+++ lib/weather_api.rb
@@ -4,20 +4,20 @@ require 'open-uri'
 module WeatherApi
 
   def self.http_get( params = {})
-    domain = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
+    domain = Rails.application.secrets['domain']
 
     params[:format] ||= 'json'
-    params[:key] ||= '66f18a0d0f064db5a2e62225182702'
-    params[:q] ||= 'London'
+    params[:key] ||= Rails.application.secrets['key']
+    params[:q] ||= Rails.application.secrets['default_location']
     params[:num_of_days] ||= '1'
 
 
     uri = URI.parse(domain)
     uri.query = URI.encode_www_form( params )
 
-    response = JSON.parse(uri.open.read)
+    response = JSON.parse(uri.open.read)["data"]["weather"] rescue ''
 
-    return response["data"]["weather"]
+    return response
   end
 
 end
