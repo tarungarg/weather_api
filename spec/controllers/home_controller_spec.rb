7d85e61706c02f94927ef711ad5072ca840ad9f2
Tarun <t.garg28@gmail.com>
2018-03-08 12:18:39 +0530

    add view spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe HomeController, type: :controller do
     context "GET #index" do
       it "should populate weather data" do
         VCR.use_cassette("weather_api") do
-          get :index
+          get :index, { days: 4 }
           expect(response).to render_template("index")
         end
       end
@@ -20,6 +20,19 @@ RSpec.describe HomeController, type: :controller do
       it "should check datatype for response" do
         expect(client).to be_an_instance_of(Array)
       end
+    end
+
+    context "Validating only 5 days allowed" do
+      subject { get :index, { days: 6 } }
+
+      it "should populate weather data" do
+        subject.should redirect_to '/'
+      end
+
+      it "should populate weather data" do
+        expect(subject).to_not render_template("index")
+      end
+
     end
   end
 
---
+++ spec/features/views/home/index_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe "view spec for home index", type: :feature do
+
+  it "should show response from weather api" do
+    visit '/'
+    fill_in 'days', with: 5
+    click_button 'Go'
+    expect(page).to have_content 'Tue Feb 27'
+  end
+
+end
