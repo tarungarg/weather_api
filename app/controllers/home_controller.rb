532663b32d17860072d15cee5c7b35eed31da5b1
Tarun <t.garg28@gmail.com>
2018-03-08 10:50:54 +0530

    added bootstrap and validation for days

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,11 @@ require 'weather_api'
 class HomeController < ApplicationController
 
   def index
-    num_of_days = params[:days]
+    if params[:days] && ![1, 2, 3, 4, 5].include?(params[:days].to_i)
+      redirect_to '/'
+      return
+    end
+    num_of_days = params[:days].to_i
     @data = WeatherApi.http_get({num_of_days: params[:days]})
 
     respond_to do |format|
